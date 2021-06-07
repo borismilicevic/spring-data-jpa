bd7d9fb7acfdcacd92da27d265655c8fce2702b9
boris.milicevic <boris31094@gmail.com>
2021-06-07 09:03:14 +0200

    End of module 3

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
@@ -1,6 +1,7 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -12,29 +13,37 @@ public class SessionRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Autowired
+    private SessionJPARepository jpaRepository;
+
     public Session create(Session session) {
-        entityManager.persist(session);
-        entityManager.flush();
-        return session;
+//        entityManager.persist(session);
+//        entityManager.flush();
+//        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public Session update(Session session) {
-        session = entityManager.merge(session);
-        entityManager.flush();
-        return session;
+//        session = entityManager.merge(session);
+//        entityManager.flush();
+//        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public void delete(Long id) {
-        entityManager.remove(find(id));
-        entityManager.flush();
+//        entityManager.remove(find(id));
+//        entityManager.flush();
+        jpaRepository.deleteById(id);
     }
 
     public Session find(Long id) {
-        return entityManager.find(Session.class, id);
+//        return entityManager.find(Session.class, id);
+        return jpaRepository.getOne(id);
     }
 
     public List<Session> list() {
-        return entityManager.createQuery("select s from Session s").getResultList();
+//        return entityManager.createQuery("select s from Session s").getResultList();
+        return jpaRepository.findAll();
     }
 
     public List<Session> getSessionsThatHaveName(String name) {
